c21f9f54a31bc27bca5e46706455ca70eab110ca
mmanjuprasad <mmanjuprasadm@gmail.com>
2021-08-08 11:56:52 +0530

    done

---
+++ app.js
@@ -0,0 +1,36 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box")
+
+function compareValues(sum, luckyNumber){
+    if(sum%luckyNumber===0){
+        outputBox.innerText = "Your Birthday is Lucky!!!!!😃";
+    }else{
+        outputBox.innerText = "Your Birthday is not so Lucky 😔";
+    }
+}
+
+function checkBirthdayIsLucky(){
+    const dob=dateOfBirth.value;
+    const sum = calculateSum(dob); 
+    if(sum && dob){
+        compareValues(sum,luckyNumber.value);
+    }else{
+        outputBox.innerText="Please enter both the feilds";
+    }
+    
+}
+
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum = 0;
+    for(let i=0; i<dob.length ; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+
+    return sum;
+}
+
+checkNumberButton.addEventListener('click',checkBirthdayIsLucky);
